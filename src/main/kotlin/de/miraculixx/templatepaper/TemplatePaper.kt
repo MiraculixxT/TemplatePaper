4035b854f465b9cd27845245b2d5f267ae0dbc87
Miraculixx <miraculixxyt@gmail.com>
2023-12-26 18:42:12 +0100

    Add template main class

---
+++ src/main/kotlin/de/miraculixx/templatepaper/TemplatePaper.kt
@@ -0,0 +1,23 @@
+package de.miraculixx.templatepaper
+
+import de.miraculixx.kpaper.main.KPaper
+
+class TemplatePaper: KPaper() {
+    companion object {
+        lateinit var INSTANCE: KPaper
+    }
+
+    override fun load() {
+        INSTANCE = this
+    }
+
+    override fun startup() {
+
+    }
+
+    override fun shutdown() {
+
+    }
+}
+
+val INSTANCE by lazy { TemplatePaper.INSTANCE }
